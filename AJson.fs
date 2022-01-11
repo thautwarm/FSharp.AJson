016fb3a860fea8270fba62e66972064dd21d09c0
Taine Zhao <twshere@outlook.com>
2022-01-11 13:13:22 +0900

    deserializations!

--- AJson.fs
+++ AJson.fs
@@ -9,6 +9,16 @@ type Json =
         | JNull
         | JList of Json ResizeArray
         | JDict of (string * Json) ResizeArray
+    with
+     member this.kind =
+        match this with
+        | JInt _ -> "int"
+        | JFloat _ -> "float"
+        | JBool _ -> "bool"
+        | JStr _ -> "string"
+        | JNull _ -> "null"
+        | JList _ -> "list"
+        | JDict _ -> "dict"
 
 module Parsec =
     type Parser<'a> = int -> string -> int * 'a
@@ -17,13 +27,14 @@ module Parsec =
             if i >= s.Length || s.[i] <> c then invalidOp "unexpected match"
             else (i + 1, ())
         apply
-    
+
     let pStr_ (pat: string) =
         let apply (i: int) (s: string) =
+
             if i + pat.Length > s.Length || s.[i..i+pat.Length - 1] <> pat then invalidOp "unexpected match"
-            else (i + 1, ())
+            else (i + pat.Length, ())
         apply
-    
+
     let pChar (c: char) =
         let apply (i: int) (s: string) =
             if i >= s.Length || s.[i] <> c then invalidOp "unexpected match"
@@ -36,7 +47,7 @@ module Parsec =
             elif Array.contains s.[i] cs then (i+1, ())
             else invalidOp "unexpected match"
         apply
-    
+
     let pCharset (cs : char array) =
         let apply (i: int) (s: string) =
             if i >= s.Length then invalidOp "unexpected match"
@@ -62,7 +73,7 @@ module Parsec =
                 | [] -> i, ()
             loop i ps
         apply
-    
+
     let pSepRep (sep: Parser<bool>) (p: Parser<'a>): Parser<'a ResizeArray> =
         let apply (i: int) (s: string) =
             let res = ResizeArray<'a>()
@@ -121,7 +132,7 @@ module Parsec =
 
     let pStr : Parser<string> =
         let apply (i: int) (s: string) =
-            if i >= s.Length && s.[i] <> '"' then
+            if i >= s.Length || s.[i] <> '"' then
                 invalidOp $"imcomplete parsing for string"
             let buf = System.Text.StringBuilder()
             let mutable find_end = false
@@ -153,7 +164,7 @@ module Parsec =
             else
                 invalidOp "incomplete string"
         apply
-    
+
     let pMap (f: 'a -> 'b) (p: Parser<'a>) : Parser<'b> =
         let apply i s =
             let i, v = p i s
@@ -162,7 +173,7 @@ module Parsec =
     let pRef (p: Parser<'a> ref): Parser<'a> =
         let apply i s = p.Value i s
         apply
-    
+
     module Json =
         let pListSep =
             pCharset [|','; ']'|]
@@ -170,7 +181,7 @@ module Parsec =
                 | ',' -> true
                 | ']' -> false
                 | _ -> failwith "impossible")
-        
+
         let pDictSep =
             pCharset [|','; '}'|]
             |> pMap (function
@@ -180,7 +191,7 @@ module Parsec =
 
         let jInt: Parser<int64> = pNumber |> pMap (fun s -> System.Int64.Parse(s))
         let jFloat: Parser<double> = pNumber |> pMap (fun s -> System.Double.Parse(s))
-        
+
         let jNumber : Parser<Json> =
             let apply (i: int) (s: string) =
                 if i >= s.Length then invalidOp "unexpected match"
@@ -194,7 +205,7 @@ module Parsec =
                     | c when c <= '9'  && c >= '0' -> loop isfloat (j + 1)
                     | _ -> isfloat, j
                 let isfloat, i = loop false i
-                let o = 
+                let o =
                     if isfloat then
                         JFloat(System.Double.Parse(s.[start..i-1]))
                     else
@@ -208,12 +219,12 @@ module Parsec =
             pStr_ "false" |> pMap (fun s -> true)
         let jNull: Parser<unit> = pStr_ "null"
         let jStr: Parser<string> = pStr
-        
+
         let refObject: Parser<(string * Json) ResizeArray> ref = ref (Unchecked.defaultof<_>)
         let jObject = pRef refObject
         let refList: Parser<Json ResizeArray> ref = ref (Unchecked.defaultof<_>)
         let jList = pRef refList
-        
+
         let json =
             la1 <| function
             | '[' -> jList |> pMap JList
@@ -225,8 +236,8 @@ module Parsec =
             | c when c >= '0' && c <= '9' -> jNumber
             | '"' -> jStr |> pMap JStr
             | c -> invalidOp (string c)
-            
-            
+
+
         refObject.Value <-
             fun (i: int) (s: string) ->
                 let i, _ = pChar_ '{' i s
@@ -240,7 +251,7 @@ module Parsec =
                     let i, v = allowSPC json i s
                     i, (key, v)
                 pSepRep pDictSep each i s
-        
+
         refList.Value <-
             fun (i: int) (s: string) ->
                 let i, _ = pChar_ '[' i s
@@ -250,7 +261,185 @@ module Parsec =
                 else
                 pSepRep pListSep (allowSPC json) i s
 
-let parseJson s = 
+let parseJson s =
     let i, o = (Parsec.allowSPC Parsec.Json.json) 0 s
     if i <> s.Length then invalidOp "json parse incomplete"
     else o
+
+
+let int64FromJson =
+    function
+    | JInt i -> i
+    | _ -> invalidOp "invalid conversion to int"
+
+let doubleFromJson =
+    function
+    | JFloat f -> f
+    | _ -> invalidOp "invalid conversion to float"
+
+let boolFromJson =
+    function
+    | JBool b -> b
+    | _ -> invalidOp "invalid conversion to bool"
+
+let stringFromJson =
+    function
+    | JStr s -> s
+    | _ -> invalidOp "invalid conversion to bool"
+
+let charFromJson =
+    function
+    | JStr s when s.Length <> 1 -> invalidOp $"invalid interpretaion from string to char"
+    | JStr s -> s.[0]
+    | _ -> invalidOp "invalid conversion to bool"
+
+
+let unitFromJson =
+    function
+    | JInt 0L -> ()
+    | _ -> invalidOp "invalid conversion to unit"
+
+
+let inline forI f (seq: ResizeArray<'a>) =
+    for i = 0 to seq.Count - 1 do
+        f i (seq.[i])
+
+let inline (==) a b = obj.ReferenceEquals(a, b)
+
+let ADT_TAG = "_TAG"
+let ADT_VALS = "_VALUES"
+
+type 'a evidence = Evidence
+
+let rec inline fromJson<'a> data =
+    unbox<'a> (objFromJson (typeof<'a>) data)
+
+and objFromJson (t: System.Type) (data: Json) =
+    if obj.ReferenceEquals(typeof<int>, t) then
+        box <| int (int64FromJson data)
+    elif obj.ReferenceEquals(typeof<int16>, t) then
+        box <| int16 (int64FromJson data)
+    elif obj.ReferenceEquals(typeof<int8>, t) then
+        box <| int8 (int64FromJson data)
+    elif obj.ReferenceEquals(typeof<int64>, t) then
+        box <| (int64FromJson data)
+    elif obj.ReferenceEquals(typeof<int8>, t) then
+        box <| int8 (int64FromJson data)
+    elif obj.ReferenceEquals(typeof<float>, t) then
+        box <| float (doubleFromJson data)
+    elif obj.ReferenceEquals(typeof<double>, t) then
+        box <| (doubleFromJson data)
+    elif obj.ReferenceEquals(typeof<bool>, t) then
+        box <| (boolFromJson data)
+    elif obj.ReferenceEquals(typeof<char>, t) then
+        let s = stringFromJson data
+        if s.Length <> 1 then invalidOp $"{s} to char"
+        else
+            box <| s.[0]
+    elif obj.ReferenceEquals(typeof<string>, t) then
+        box <| stringFromJson data
+    elif t.IsArray then
+        let eltype = t.GetElementType()
+        let seq =
+            match data with
+            | JList seq -> seq
+            | _ -> invalidOp $"convert {data.kind} to {t}"
+        // integers
+        if eltype == typeof<int> then
+            box <| Array.init seq.Count (fun i -> int (int64FromJson seq.[i]))
+        elif eltype == typeof<int64> then
+            box <| Array.init seq.Count (fun i -> (int64FromJson seq.[i]))
+        elif eltype == typeof<int16> then
+            box <| Array.init seq.Count (fun i -> int16 (int64FromJson seq.[i]))
+        elif eltype == typeof<int8> then
+            box <| Array.init seq.Count (fun i -> int8 (int64FromJson seq.[i]))
+        elif eltype == typeof<uint32> then
+            box <| Array.init seq.Count (fun i -> Core.Operators.uint32 (int64FromJson seq.[i]))
+        elif eltype == typeof<uint64> then
+            box <| Array.init seq.Count (fun i -> Core.Operators.uint64 (int64FromJson seq.[i]))
+        elif eltype == typeof<uint8> then
+            box <| Array.init seq.Count (fun i -> Core.Operators.byte (int64FromJson seq.[i]))
+        elif eltype == typeof<uint16> then
+            box <| Array.init seq.Count (fun i -> Core.Operators.uint16 (int64FromJson seq.[i]))
+
+        // floats
+        elif eltype == typeof<float> then
+            box <| Array.init seq.Count (fun i -> float (doubleFromJson seq.[i]))
+        elif eltype == typeof<double> then
+            box <| Array.init seq.Count (fun i -> (doubleFromJson seq.[i]))
+        elif eltype == typeof<decimal>  then
+            box <| Array.init seq.Count (fun i -> decimal (doubleFromJson seq.[i]))
+        // others
+        elif eltype == typeof<string> then
+            box <| Array.init seq.Count (fun i -> (stringFromJson seq.[i]))
+        elif eltype == typeof<bool> then
+            box <| Array.init seq.Count (fun i -> (boolFromJson seq.[i]))
+        elif eltype == typeof<unit> then
+            box <| Array.init seq.Count (fun i -> (unitFromJson seq.[i]))
+        elif eltype == typeof<char> then
+            box <| Array.init seq.Count (fun i -> (charFromJson seq.[i]))
+        else
+            box <| Array.init seq.Count (fun i -> objFromJson eltype seq.[i])
+        // let array = System.Array.CreateInstance(eltype, seq.Count)
+        // for i = 0 to seq.Count - 1 do
+        //     array.SetValue(objFromJson eltype (seq.[i]), i)
+        // box <| array
+    elif t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<option<_>> then
+        let eltype = t.GenericTypeArguments.[0]
+        match data with
+        | JNull -> None: obj
+        | _ -> Some (objFromJson eltype data)
+    elif t.IsGenericType && t.GetGenericTypeDefinition() = typedefof<list<_>> then
+        let eltype = t.GenericTypeArguments.[0]
+        let mutable o: obj list = []
+        let seq =
+            match data with
+            | JList seq -> seq
+            | _ -> invalidOp $"convert {data.kind} to {t}"
+        for i = seq.Count - 1 downto 0 do
+            o <- objFromJson eltype seq.[i] :: o
+        box <| o
+    elif FSharpType.IsRecord t then
+        let fields = FSharpType.GetRecordFields t
+                     |> Array.map (fun f -> f.Name, f.PropertyType)
+        let arguments = Array.create fields.Length (null: obj)
+        let pairs =
+            match data with
+            | JDict pairs -> pairs
+            | _ -> invalidOp $"convert {data.kind} to {t}"
+        for (k, v) in pairs do
+            let i = Array.findIndex (fun (fname, _) -> k = fname) fields
+            let (_, fieldt) = fields.[i]
+            arguments.[i] <- objFromJson fieldt v
+        FSharpValue.MakeRecord(t, arguments)
+    elif FSharpType.IsTuple t then
+        let eltypes = FSharpType.GetTupleElements t
+        let seq =
+            match data with
+            | JList seq -> seq
+            | _ -> invalidOp $"convert {data.kind} to {t}"
+        FSharpValue.MakeTuple(
+            Array.init seq.Count (fun i -> objFromJson eltypes.[i] seq.[i]), t)
+    elif FSharpType.IsUnion t then
+        let pairs =
+            match data with
+            | JDict pairs -> pairs
+            | _ -> invalidOp $"convert {data.kind} to {t}"
+        let _, tag = Seq.find (fun (k, _) -> k = ADT_TAG) pairs
+        let tag = stringFromJson tag
+        let _, values = Seq.find (fun (k, _) -> k = ADT_VALS) pairs
+        let case =
+            FSharpType.GetUnionCases t
+            |> Array.find (fun case -> case.Name = tag)
+        let fieldtypes = case.GetFields() |> Array.map (fun f -> f.PropertyType)
+        let values =
+            match values with
+            | JList values -> Array.init values.Count (fun i -> objFromJson fieldtypes.[i] values.[i])
+            | _ -> invalidOp $"convert {data.kind} to {t}"
+        FSharpValue.MakeUnion(case, values)
+    else
+        invalidOp $"unsupported data type fromJson: {t}"
+
+let inline deserialize<'a> s =
+    let json = parseJson s
+    objFromJson typeof<'a> json :?> 'a
